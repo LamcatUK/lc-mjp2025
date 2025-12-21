c9370ac6a53431fc23220261d59c9b43a135cf1c
LamcatUK <git@lamcat.co.uk>
2025-12-21 17:58:00 +0000

    column cards

--- blocks/lc-column-cards.php
+++ blocks/lc-column-cards.php
@@ -7,20 +7,22 @@
 
 defined( 'ABSPATH' ) || exit;
 
-$bg      = get_field( 'bg_colour' );
-$fg      = get_field( 'fg_colour' );
-$cols    = get_field( 'columns' ) ? get_field( 'columns' ) : 'three';
-$classes = array();
-$style   = '';
+// Support Gutenberg color picker.
+$bg = ! empty( $block['backgroundColor'] ) ? 'has-' . $block['backgroundColor'] . '-background-color' : '';
+$fg = ! empty( $block['textColor'] ) ? 'has-' . $block['textColor'] . '-color' : '';
 
+$classes = array();
 if ( $bg ) {
-	$classes[] = 'has-' . sanitize_html_class( $bg ) . '-background-color';
+	$classes[] = sanitize_html_class( $bg );
 }
 if ( $fg ) {
-	$classes[] = 'has-' . sanitize_html_class( $fg ) . '-color';
+	$classes[] = sanitize_html_class( $fg );
 }
+
+$cols = get_field( 'columns' ) ? get_field( 'columns' ) : 'three';
+
 ?>
-<section class="col-cards <?php echo esc_attr( implode( ' ', $classes ) ); ?>">
+<section class="col-cards <?= esc_attr( implode( ' ', $classes ) ); ?>">
 	<div class="container py-5">
 		<h2><?= wp_kses_post( get_field( 'title' ) ); ?></h2>
 		<div class="col-cards__intro w-constrained-md mb-4"><?= esc_html( get_field( 'intro' ) ); ?></div>
@@ -34,8 +36,8 @@ if ( $fg ) {
 					<div class="<?= esc_attr( 'two' === $cols ? 'col-12 col-md-6' : 'col-12 col-md-4' ); ?>">
 						<?php if ( $card_link ) : ?>
 							<a href="<?= esc_url( $card_link['url'] ); ?>" class="col-card col-card--linked" 
-							   <?= $card_link['target'] ? 'target="' . esc_attr( $card_link['target'] ) . '"' : ''; ?>
-							   <?= $card_link['title'] ? 'title="' . esc_attr( $card_link['title'] ) . '"' : ''; ?>>
+								<?= $card_link['target'] ? 'target="' . esc_attr( $card_link['target'] ) . '"' : ''; ?>
+								<?= $card_link['title'] ? 'title="' . esc_attr( $card_link['title'] ) . '"' : ''; ?>>
 								<h3 class="fs-subtle"><?= wp_kses_post( get_sub_field( 'title' ) ); ?></h3>
 								<div class="fs-body fw-thin"><?= wp_kses_post( get_sub_field( 'content' ) ); ?></div>
 								<span class="col-card__arrow" aria-hidden="true"><i class="fas fa-chevron-right fa-2x"></i></span>
