65ec5ff6485259d4d6d85a32a8be5b986fd94211
LamcatUK <git@lamcat.co.uk>
2026-01-10 15:52:13 +0000

    recent projects

--- blocks/lc-recent-projects.php
+++ blocks/lc-recent-projects.php
@@ -7,11 +7,11 @@
 
 defined( 'ABSPATH' ) || exit;
 
-$work_type = get_field( 'work_type' ); // or get from query var, etc.
+$work_type = get_field( 'work_type' );
 
 $args = array(
-	'post_type'      => 'attachment',
-	'post_status'    => 'inherit',
+	'post_type'      => 'casestudy',
+	'post_status'    => 'publish',
 	'posts_per_page' => 12,
 	'orderby'        => 'date',
 	'order'          => 'DESC',
@@ -51,6 +51,51 @@ if ( $fg ) {
 $query = new WP_Query( $args );
 
 if ( $query->have_posts() ) {
+	// Collect all images from case studies
+	$all_images = array();
+	while ( $query->have_posts() ) {
+		$query->the_post();
+		$gallery = get_field( 'case_study_gallery', get_the_ID() );
+		if ( $gallery && is_array( $gallery ) && ! empty( $gallery ) ) {
+			$all_images[] = array(
+				'images' => $gallery,
+				'post_url' => get_permalink( get_the_ID() ),
+			);
+		}
+	}
+	wp_reset_postdata();
+
+	// Build final image list with minimum 8 images
+	$final_images = array();
+	$min_images = 8;
+	
+	if ( ! empty( $all_images ) ) {
+		$case_study_count = count( $all_images );
+		$current_index = 0;
+		
+		// Interleave images from all case studies
+		while ( count( $final_images ) < $min_images ) {
+			$case_study = $all_images[ $current_index % $case_study_count ];
+			
+			// Get next available image from this case study
+			$image_index = floor( $current_index / $case_study_count ) % count( $case_study['images'] );
+			$image_id = $case_study['images'][ $image_index ];
+			
+			$final_images[] = array(
+				'image_id' => $image_id,
+				'post_url' => $case_study['post_url'],
+			);
+			
+			$current_index++;
+			
+			// Safety check to prevent infinite loop
+			if ( $current_index > $min_images * $case_study_count * 10 ) {
+				break;
+			}
+		}
+	}
+
+	if ( ! empty( $final_images ) ) {
 	?>
 <section class="recent-projects py-5 <?= esc_attr( implode( ' ', $classes ) ); ?>">
 	<div class="container">
@@ -67,25 +112,21 @@ if ( $query->have_posts() ) {
 		}
 		echo '<div class="recent-projects__attachments swiper mb-4">';
 		echo '<div class="swiper-wrapper pt-2 pb-3">';
-		while ( $query->have_posts() ) {
-			$query->the_post();
-			$terms     = get_the_terms( get_the_ID(), 'work_type' );
-			$term_slug = '';
-			if ( $terms && ! is_wp_error( $terms ) && ! empty( $terms ) ) {
-				$term_slug = $terms[0]->slug;
-			}
-			echo '<a class="swiper-slide image-16x9" href="/projects/?work_type=' . esc_attr( $term_slug ) . '">';
-			echo wp_get_attachment_image( get_the_ID(), 'medium', false, array( 'class' => 'img-fluid mb-3' ) );
+
+		foreach ( $final_images as $item ) {
+			echo '<a class="swiper-slide image-16x9" href="' . esc_url( $item['post_url'] ) . '">';
+			echo wp_get_attachment_image( $item['image_id'], 'medium', false, array( 'class' => 'img-fluid mb-3' ) );
 			echo '</a>';
 		}
+		
 		echo '</div>';
 		echo '</div>';
-		wp_reset_postdata();
 		?>
 		<div class="text-center"><a href="/projects/" class="btn btn--primary">View more projects</a></div>
 	</div>
 </section>
 	<?php
+	}
 }
 add_action(
 	'wp_footer',
